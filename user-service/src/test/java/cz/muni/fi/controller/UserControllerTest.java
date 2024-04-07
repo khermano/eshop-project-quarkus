b945d05bb3278deef08e1567551968b7f03741d0
khermano <khermano@redhat.com>
2024-04-07 23:13:34 +0200

    [user-service] Finalizing UserControllerTest

--- user-service/src/test/java/cz/muni/fi/controller/UserControllerTest.java
+++ user-service/src/test/java/cz/muni/fi/controller/UserControllerTest.java
@@ -6,13 +6,13 @@ import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.MockitoConfig;
+import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
@@ -32,56 +32,10 @@ public class UserControllerTest {
     @MockitoConfig(convertScopes = true)
     private UserController usersController;
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
-    }
+    private static List<User> users;
 
-    @Test
-    public void getAllUsers() {
-
-        doReturn(panacheQuery).when(userRepository).findAll();
-        doReturn(Collections.unmodifiableList(this.createUsers())).when(panacheQuery).list();
-
-
-        given()
-                .when().get("/users")
-                .then()
-                .statusCode(200)
-                .body("size()", is(2))
-                .body("id", hasItems(1, 2))
-                .body("surname", hasItems("Smith", "Williams"));
-    }
-
-//    @Test
-//    public void getValidUser() throws Exception {
-//        List<Optional<User>> users = this.createUsers();
-//
-//        doReturn(users.get(0)).when(userRepository).findById(1L);
-//        doReturn(users.get(1)).when(userRepository).findById(2L);
-//
-//        mockMvc.perform(get("/1"))
-//                .andExpect(status().isOk())
-//                .andExpect(
-//                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
-//                .andExpect(jsonPath("$.surname").value("Smith"));
-//
-//        mockMvc.perform(get("/2"))
-//                .andExpect(status().isOk())
-//                .andExpect(
-//                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
-//                .andExpect(jsonPath("$.surname").value("Williams"));
-//    }
-//
-//    @Test
-//    public void getInvalidUser() throws Exception {
-//        doReturn(Optional.empty()).when(userRepository).findById(1L);
-//
-//        mockMvc.perform(get("/1"))
-//                .andExpect(status().is4xxClientError());
-//    }
-
-    private List<User> createUsers() {
+    @BeforeAll
+    public static void usersSetup() {
         User userOne = new User();
         userOne.setId(1L);
         userOne.setGivenName("John");
@@ -91,7 +45,57 @@ public class UserControllerTest {
         userTwo.setId(2L);
         userTwo.setGivenName("Mary");
         userTwo.setSurname("Williams");
-        
-        return Arrays.asList(userOne, userTwo);
+
+        users = List.of(userOne, userTwo);
+    }
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void getAllUsers() {
+        doReturn(panacheQuery).when(userRepository).findAll();
+        doReturn(users).when(panacheQuery).list();
+
+
+        given()
+                .when().get("/users")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", hasItems(1, 2))
+                .body("surname", hasItems("Smith", "Williams"));
+    }
+
+    @Test
+    public void getValidUser() {
+        doReturn(users.get(0)).when(userRepository).findById(1L);
+        doReturn(users.get(1)).when(userRepository).findById(2L);
+
+        given()
+                .when().get("/users/1")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("surname", is("Smith"));
+
+        given()
+                .when().get("/users/2")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("surname", is("Williams"));
+    }
+
+    @Test
+    public void getInvalidUser() {
+        doReturn(null).when(userRepository).findById(1L);
+
+        given()
+                .when().get("/users/1")
+                .then()
+                .statusCode(404);
     }
 }
