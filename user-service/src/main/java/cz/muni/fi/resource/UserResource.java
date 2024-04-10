256ed98a0abac46da2b3e35280678bbbc31ece29
khermano <khermano@redhat.com>
2024-04-10 16:06:44 +0200

    [user-service] Using Response

--- user-service/src/main/java/cz/muni/fi/resource/UserResource.java
+++ user-service/src/main/java/cz/muni/fi/resource/UserResource.java
@@ -4,13 +4,12 @@ import cz.muni.fi.entity.User;
 import cz.muni.fi.repository.UserRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Collection;
 
 /**
  * REST Controller for Users
@@ -30,10 +29,10 @@ public class UserResource {
      * @return list of Users
      */
     @GET
-    public Collection<User> getUsers() {
+    public Response getUsers() {
         logger.debug("rest getUsers()");
 
-        return userRepository.findAll().list();
+        return Response.ok(userRepository.findAll().list()).build();
     }
 
     /**
@@ -41,19 +40,19 @@ public class UserResource {
      * e.g.: curl -i -X GET http://localhost:8080/eshop-rest/users/1
      * 
      * @param id of the user
-     * @return User with given id
+     * @return User with given id or 404
      */
     @GET
     @Path("{id}")
-    public User getUser(long id) {
+    public Response getUser(long id) {
         logger.debug("rest getUser({})", id);
 
         User user = userRepository.findById(id);
         if (user != null) {
-            return user;
+            return Response.ok(user).build();
         }
         else {
-            throw new NotFoundException("The requested resource was not found");
+            return Response.status(404,"The requested resource was not found").build();
         }
     }
 }
