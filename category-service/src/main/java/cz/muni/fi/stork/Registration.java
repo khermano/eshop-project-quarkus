6ce7d8036e89ab1265e23b9ed3b84cb13d0f47e1
khermano <khermano@redhat.com>
2024-04-14 17:05:32 +0200

    [category-service] Single point of failure - service needed to have Consul running for test to run successfully, but not anymore

--- category-service/src/main/java/cz/muni/fi/stork/Registration.java
+++ category-service/src/main/java/cz/muni/fi/stork/Registration.java
@@ -1,10 +1,12 @@
 package cz.muni.fi.stork;
 
+import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.ext.consul.ServiceOptions;
 import io.vertx.mutiny.ext.consul.ConsulClient;
 import io.vertx.ext.consul.ConsulClientOptions;
 import io.vertx.mutiny.core.Vertx;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
@@ -17,15 +19,21 @@ public class Registration {
 
     @ConfigProperty(name = "categories-port", defaultValue = "8082") int categories;
 
+    @Inject
+    LaunchMode mode;
     /**
      * Register our category service in Consul.
      *
      * Note: this method is called on a worker thread, and so it is allowed to block.
      */
     public void init(@Observes StartupEvent ev, Vertx vertx) {
-        ConsulClient client = ConsulClient.create(vertx, new ConsulClientOptions().setHost(host).setPort(port));
+        if (mode != LaunchMode.TEST) { //https://quarkus.io/guides/lifecycle#launch-modes - padali mi bez toho testy
+//        System.out.println(mode.name());
+            ConsulClient client = ConsulClient.create(vertx, new ConsulClientOptions().setHost(host).setPort(port));
 
-        client.registerServiceAndAwait(
-                new ServiceOptions().setPort(categories).setAddress("localhost").setName("categories"));
+            client.registerServiceAndAwait(
+                    new ServiceOptions().setPort(categories).setAddress("localhost").setName("categories"));
+        }
+
     }
 }
