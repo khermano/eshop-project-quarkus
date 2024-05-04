a8d391895e89307b8889e455c219f13c6727c518
khermano <khermano@redhat.com>
2024-05-04 13:50:53 +0200

    Removing unused methods, changing price generation to fixed value

--- category-service/src/main/java/cz/muni/fi/repository/CategoryRepository.java
+++ category-service/src/main/java/cz/muni/fi/repository/CategoryRepository.java
@@ -1,12 +1,9 @@
 package cz.muni.fi.repository;
 
 import cz.muni.fi.entity.Category;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class CategoryRepository implements PanacheRepository<Category> {
-	public Category findByName(String name) {
-		return find("name", name).firstResult();
-	}
+public class CategoryRepository implements PanacheRepositoryBase<Category, Long> {
 }
